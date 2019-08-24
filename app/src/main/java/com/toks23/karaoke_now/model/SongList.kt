01475b56d2081caba62dce391db66045dfd06ddf
tanie <tanie_calacar@yahoo.com>
2019-08-24 21:47:51 +0800

    modifiy wificommandservice and wifibroadcastreceiver

---
+++ app/src/main/java/com/toks23/karaoke_now/model/SongList.kt
@@ -0,0 +1,19 @@
+package com.toks23.karaoke_now.model
+
+class SongList {
+
+    var songId: Int = 0
+    var songFilename: String = ""
+    var songTitle: String = ""
+    var songArtist: String = ""
+    var songFilePath: String = ""
+
+    /*
+    fun get_favorites(): Int {
+        return m_favorites
+    }
+
+    fun set_favorites(favorites: Int) {
+        this.m_favorites = favorites
+    }*/
+}
