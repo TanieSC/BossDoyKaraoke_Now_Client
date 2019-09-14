36b681f5b5ed8762dd28d163dd324d6f5c46c7a6
tanie <tanie_calacar@yahoo.com>
2019-09-14 21:52:53 +0800

    add listview

--- app/src/main/java/com/toks23/karaoke_now/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/toks23/karaoke_now/ui/main/PlaceholderFragment.kt
@@ -26,7 +26,7 @@ import java.io.*
  */
 class PlaceholderFragment : Fragment(), SearchView.OnQueryTextListener, AdapterView.OnItemClickListener  {
 
-    lateinit var recyclerView : RecyclerView
+    private lateinit var _recyclerView : RecyclerView
 
     override fun onQueryTextSubmit(query: String?): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -56,118 +56,117 @@ class PlaceholderFragment : Fragment(), SearchView.OnQueryTextListener, AdapterV
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
 
         pageViewModel.index.observe(this, Observer{
             pageViewModel.songs.observe(this, Observer {data ->
                 when (it) {
                     0 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     1 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     2 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     3 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     4 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     5 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     6 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     7 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     8 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     9 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     10 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     11 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     12 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     13 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     14 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     15 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     16 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     17 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     18 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     19 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     20 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     21 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     22 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     23 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     24 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     25 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                     26 -> {
-                        recyclerView = root.findViewById(R.id.songList) as RecyclerView
-                        setupRecyclerView(recyclerView, data[it])
+                        _recyclerView = root.findViewById(R.id.songList) as RecyclerView
+                        setupRecyclerView(_recyclerView, data[it])
                     }
                 }
             })
